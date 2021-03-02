e54694dde8064de7fb7ffe18c99e3691c6cf657e
Phyzon21 <jjRabino@mcm.edu.ph>
2021-03-02 14:36:03 +0800

    initial commit

---
+++ Dead Camp/Assets/Scripts/TriggerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerTest : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+
+            this.gameObject.SetActive(false);
+            SceneManager.LoadScene("Death");
+            //Destroy(gameObject);
+        }
+    }
+}
